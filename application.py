bb72771c872952f25bf067e5ce696fbe44e23d43
krassykirov <krassy.kirov@gmail.com>
2019-11-17 21:47:56 +0200

    changes

--- application.py
+++ application.py
@@ -63,9 +63,10 @@ def key_vault():
         endpoint = "https://uaekeyvault.vault.azure.net/secrets/mysecret?api-version=2016-10-01"
         headers = {"Authorization": 'Bearer {}'.format(access_token)}
         response = requests.get(endpoint, headers=headers).json()
+        response = response['value']
         return render_template(
             'KeyVault.html',
-            response= 'The response is: {} '.format(response)
+             message= 'The response is: {} '.format(response)
         )
     except Exception as error:
         return render_template(
