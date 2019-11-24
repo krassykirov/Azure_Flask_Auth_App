246668723cbb9f7fa71b991246b400cf85bc0692
krassykirov <krassy.kirov@gmail.com>
2019-11-24 21:31:07 +0200

    fixing issues

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 CLIENT_ID = '6be1ec05-2113-4f92-a179-84eb90d05d00'
 CLIENT_SECRET = "[7WgmM=Q78Qr?I2nK[R@qDQhe-:sja1x"
-REDIRECT_URI = 'http://localhost:5000/login/authorized'  #'https://krassy3.azurewebsites.net/login/authorized'
+REDIRECT_URI = 'https://krassy3.azurewebsites.net/login/authorized'  #'https://krassy3.azurewebsites.net/login/authorized'
 AUTHORITY_URL = 'https://login.microsoftonline.com/common'
 AUTH_ENDPOINT = '/oauth2/v2.0/authorize'
 TOKEN_ENDPOINT = '/oauth2/v2.0/token'
